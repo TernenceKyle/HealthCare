b6856a2944f08f58d62074b820083e4dadc1bfeb
git <2970919054@qq.com>
2021-02-01 09:30:40 +0800

    NotCommit

--- hc_parent/hc_backend/src/main/java/com/jadenyee/controller/AuthorityController.java
+++ hc_parent/hc_backend/src/main/java/com/jadenyee/controller/AuthorityController.java
@@ -4,6 +4,7 @@ import com.jadenyee.constant.MessageConstant;
 import com.jadenyee.entity.PageResult;
 import com.jadenyee.entity.QueryPageBean;
 import com.jadenyee.entity.Result;
+import com.jadenyee.pojo.Permission;
 import com.jadenyee.pojo.Role;
 import com.jadenyee.pojo.User;
 import com.jadenyee.service.AuthorityService;
@@ -23,6 +24,10 @@ import java.util.List;
 public class AuthorityController {
     @Reference
     AuthorityService authorityService;
+
+    /**
+     * 用户管理
+     */
 
     //分页查询用户信息
     @RequestMapping("/userList")
@@ -53,6 +58,18 @@ public class AuthorityController {
             return new Result(false,MessageConstant.QUERY_ROLE_FAIL);
         }
     }
+    //根据id查询角色信息
+    @RequestMapping("/findAllUser")
+    public Result findAllUser(Integer id){
+        try {
+                User user = authorityService.findAllUser(id);
+            return new Result(true, MessageConstant.QUERY_USER_SUCCESS,user);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.QUERY_USER_FAIL);
+        }
+    }
+
     //查询用户拥有的角色
     @RequestMapping("/findRolesByUser")
     public Result findRolesByUser(Integer id){
@@ -64,7 +81,19 @@ public class AuthorityController {
             return new Result(false,MessageConstant.QUERY_ROLE_FAIL);
         }
     }
+    //编辑用户信息
+    @RequestMapping("/editUser")
+    public Result editUser (@RequestBody User user){
+        try {
+            authorityService.editUser(user);
+            return new Result(true,MessageConstant.EDIT_USER_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.EDIT_USER_FAIL);
+        }
+    }
 
+    //修改用户的角色
     @RequestMapping("/userRoleAdd")
     public Result userRoleAdd (Integer userId,@RequestBody List<Integer> roleId){
         try {
@@ -75,4 +104,111 @@ public class AuthorityController {
             return new Result(false,MessageConstant.ADD_USER_ROLE_FAIL);
         }
     }
+    //删除用户信息
+    @RequestMapping("/deleteUser")
+    public Result deleteUser (Integer id){
+        try {
+            authorityService.deleteUser(id);
+            return new Result(true,MessageConstant.DELETE_USER_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.DELETE_USER_FAIL);
+        }
+    }
+
+    /**
+     * 角色管理
+     */
+    //查询角色列表
+    @RequestMapping("/roleList")
+    public Result roleList (){
+        try {
+            List<Role> allRole = authorityService.findAllRole();
+            return new Result(true,MessageConstant.QUERY_ROLE_SUCCESS,allRole);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.QUERY_ROLE_FAIL);
+        }
+    }
+    //查询全部的权限
+    @RequestMapping("/findAllPermission")
+    public Result findAllPermission(){
+        try {
+            List<Permission> permission = authorityService.findAllPermission();
+            return new Result(true, MessageConstant.QUERY_PERMISSION_SUCCESS,permission);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.QUERY_PERMISSION_FAIL);
+        }
+    }
+    //通过角色id查询拥有的权限id
+    @RequestMapping("/findPermissionsByRole")
+    public Result findPermissionsByRole(Integer id){
+        try {
+            List<Integer> permissions = authorityService.findPermissionsByRole(id);
+            return new Result(true,MessageConstant.QUERY_PERMISSION_SUCCESS,permissions);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.QUERY_PERMISSION_FAIL);
+        }
+    }
+    //修改角色的权限
+    @RequestMapping("/rolePermissionAdd")
+    public Result rolePermissionAdd (Integer roleId,@RequestBody List<Integer> permissionIds){
+        try {
+            authorityService.editRolePermission(roleId,permissionIds);
+            return new Result(true,MessageConstant.ADD_ROLE_PERMISSION_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.ADD_ROLE_PERMISSION_FAIL);
+        }
+    }
+
+    /**
+     * 权限管理
+     */
+    //分页查询权限信息
+    @RequestMapping("/permissionList")
+    public PageResult permissionList(@RequestBody QueryPageBean pageBean){
+        return authorityService.findPermissionPage(pageBean);
+    }
+    //新建权限
+    @RequestMapping("/PermissionAdd")
+    public Result PermissionAdd(@RequestBody Permission permission){
+        try {
+            authorityService.PermissionAdd(permission);
+            return new Result(true, MessageConstant.ADD_PERMISSION_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.ADD_PERMISSION_FAIL);
+        }
+    }
+    //根据id查询权限
+    @RequestMapping("/getPermission")
+    public Permission getPermission(Integer id){
+        return authorityService.getPermission(id);
+    }
+    //根据修改权限信息
+    @RequestMapping("/updatePermission")
+    public Result updatePermission(@RequestBody Permission permission){
+        try {
+            authorityService.updatePermission(permission);
+            return new Result(true, MessageConstant.EDIT_PERMISSION_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.EDIT_PERMISSION_FAIL);
+        }
+    }
+    //根据id删除权限信息
+    @RequestMapping("/deletePermission")
+    public Result deletePermission(Integer id){
+        try {
+            authorityService.deletePermission(id);
+            return new Result(true, MessageConstant.DELETE_PERMISSION_SUCCESS);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.DELETE_PERMISSION_FAIL);
+        }
+    }
+
 }
--- hc_parent/hc_service_provider/src/main/java/com/jadenyee/service/AuthorityServiceImpl.java
+++ hc_parent/hc_service_provider/src/main/java/com/jadenyee/service/AuthorityServiceImpl.java
@@ -6,6 +6,7 @@ import com.jadenyee.dao.AuthorityDao;
 import com.jadenyee.entity.PageResult;
 import com.jadenyee.entity.QueryPageBean;
 import com.jadenyee.entity.Result;
+import com.jadenyee.pojo.Permission;
 import com.jadenyee.pojo.Role;
 import com.jadenyee.pojo.User;
 import org.apache.dubbo.config.annotation.Service;
@@ -47,6 +48,10 @@ public class AuthorityServiceImpl implements AuthorityService {
         String newPassword = passwordEncoder.encode(password);
         //将加后的密码写入
         user.setPassword(newPassword);
+        //判断用户状态
+        if (user.getStation() == null){
+            user.setStation("0");
+        }
         authorityDao.userAdd(user);
     }
     //查询所有的角色
@@ -62,12 +67,49 @@ public class AuthorityServiceImpl implements AuthorityService {
         return roles;
     }
 
+    //查询全部权限
+    @Override
+    public List<Permission> findAllPermission() {
+        return authorityDao.findAllPermission();
+    }
+
+    //通过角色id查询拥有的权限id
+    @Override
+    public List<Integer> findPermissionsByRole(Integer id) {
+        return authorityDao.findPermissionsByRole(id);
+    }
+
+    //根据id查询用户
+    @Override
+    public User findAllUser(Integer id) {
+        return authorityDao.findAllUser(id);
+    }
+
+    //编辑用户信息
+    @Override
+    public void editUser(User user) {
+        //用户密码
+        String password = user.getPassword();
+        //密码加密
+        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        //加密
+        String newPassword = passwordEncoder.encode(password);
+        //将加后的密码写入
+        user.setPassword(newPassword);
+        //判断用户状态
+        if (user.getStation() == null){
+            user.setStation("0");
+        }
+        authorityDao.editUser(user);
+    }
+
     //修改用户的角色
     @Override
     public void editUserRole(Integer userId, List<Integer> roleIds) {
-        //删除用户原本拥有的角色
+        //查询用户原本拥有的角色
         List<Integer> roles = authorityDao.findRolesByUser(userId);
         if (roles != null && roles.size() > 0){
+            //删除用户原本拥有的角色
             authorityDao.deleteUserRole(userId);
         }
         //添加用户的新角色
@@ -77,6 +119,75 @@ public class AuthorityServiceImpl implements AuthorityService {
             }
         }
     }
+    @Override
+    public void deleteUser(Integer id) {
+        //查询用户原本拥有的角色
+        List<Integer> roles = authorityDao.findRolesByUser(id);
+        if (roles != null && roles.size() > 0){
+            //删除用户原本拥有的角色
+            authorityDao.deleteUserRole(id);
+        }
+        //删除用户
+        authorityDao.deleteUser(id);
+    }
+
+    //修改角色的权限
+    @Override
+    public void editRolePermission(Integer roleId, List<Integer> permissions) {
+        //删除角色原本拥有的权限
+        List<Integer> oldPer = authorityDao.findPermissionsByRole(roleId);
+        if (oldPer != null && oldPer.size() > 0){
+            authorityDao.deleteRolePermission(roleId);
+        }
+        //添加角色的新权限
+        if (permissions.size() > 0){
+            for (Integer perId : permissions) {
+                authorityDao.addRolePermission(roleId, perId);
+            }
+        }
+    }
+
+    //查询权限列表数据
+    @Override
+    public PageResult findPermissionPage(QueryPageBean queryPageBean) {
+        String queryString = queryPageBean.getQueryString();
+        Integer currentPage = queryPageBean.getCurrentPage();
+        Integer pageSize = queryPageBean.getPageSize();
+        PageHelper.startPage(currentPage,pageSize);
+        Page<Permission> permissionList = authorityDao.findPermissionList(queryString);
+        return new PageResult(permissionList.getTotal(),permissionList.getResult());
+    }
+
+
+
+    //添加权限
+    @Override
+    public void PermissionAdd(Permission permission) {
+        authorityDao.permissionAdd(permission);
+    }
+
+    //根据权限id查询单条权限
+    @Override
+    public Permission getPermission(Integer id) {
+        return authorityDao.getPermission(id);
+    }
+
+    //修改权限
+    @Override
+    public void updatePermission(Permission permission) {
+        authorityDao.updatePermission(permission);
+    }
+
+    //删除权限
+    @Override
+    public void deletePermission(Integer id) {
+        //删除角色_权限表中的对应数据
+        authorityDao.deletePermissionOne(id);
+        //删除权限数据
+        authorityDao.deletePermissionTwo(id);
+    }
+
+
 
 
 }
